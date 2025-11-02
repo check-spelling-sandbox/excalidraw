1ccba95df28aa42f04f08ba562f7902c144ba5b2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-01 22:37:06 -0400

    spelling: events
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -45,7 +45,7 @@ export const POINTER_EVENTS = {
   enabled: "all",
   disabled: "none",
   // asserted as any so it can be freely assigned to React Element
-  // "pointerEnvets" CSS prop
+  // "pointerEvents" CSS prop
   inheritFromUI: "var(--ui-pointerEvents)" as any,
 } as const;
 
