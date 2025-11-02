644f558d1208679c82b87608338ee12e66c8e4da
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-01 22:54:03 -0400

    spelling: mimicking
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/excalidraw/tests/history.test.tsx
+++ packages/excalidraw/tests/history.test.tsx
@@ -3983,7 +3983,7 @@ describe("history", () => {
           elements: [
             newElementWith(h.elements[0], {
               boundElements: [{ id: remoteText.id, type: "text" }],
-              isDeleted: false, // purposefully undeleting, mimicing concurrent update
+              isDeleted: false, // purposefully undeleting, mimicking concurrent update
             }),
             h.elements[1],
             // rebinding the container with a new text element!
