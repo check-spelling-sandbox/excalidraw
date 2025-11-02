5e2c2b8d97e31eb4e449541005054d4da0f6f136
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-01 23:21:58 -0400

    spelling: slightly
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/excalidraw/subset/woff2/woff2-bindings.ts
+++ packages/excalidraw/subset/woff2/woff2-bindings.ts
@@ -2,7 +2,7 @@
 // @ts-nocheck
 
 /**
- * Slitghly modified emscripten bindings of https://github.com/kekee000/fonteditor-core/blob/582bba757aa2915ec2240c61717ff12c50594675/woff2src/woff2.js
+ * Slightly modified emscripten bindings of https://github.com/kekee000/fonteditor-core/blob/582bba757aa2915ec2240c61717ff12c50594675/woff2src/woff2.js
  *
  * CHANGELOG:
  * - replaced existing exports with default esm export (`export default Module;`)
