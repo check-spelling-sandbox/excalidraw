8e831555e26ab7cc05d6889fe51c3a0acb875530
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-01 22:55:40 -0400

    spelling: offsetting
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/element/src/renderElement.ts
+++ packages/element/src/renderElement.ts
@@ -713,7 +713,7 @@ export const renderSelectionElement = (
 
   // render from 0.5px offset  to get 1px wide line
   // https://stackoverflow.com/questions/7530593/html5-canvas-and-line-width/7531540#7531540
-  // TODO can be be improved by offseting to the negative when user selects
+  // TODO can be be improved by offsetting to the negative when user selects
   // from right to left
   const offset = 0.5 / appState.zoom.value;
 
