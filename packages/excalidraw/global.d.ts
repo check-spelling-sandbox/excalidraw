cf8ae3b460dd089a94a2fb0c3e6f1e8518a80cae
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-01 22:03:36 -0400

    spelling: access
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/excalidraw/global.d.ts
+++ packages/excalidraw/global.d.ts
@@ -52,7 +52,7 @@ declare module "png-chunks-extract" {
 // -----------------------------------------------------------------------------
 
 interface Blob {
-  handle?: import("browser-fs-acces").FileSystemHandle;
+  handle?: import("browser-fs-access").FileSystemHandle;
   name?: string;
 }
 
