07a63cc5d4e6ea08eb95fe00fbb3c830dd201d70
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-26 14:26:34 -0500

    spelling: multipoint
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/excalidraw/actions/actionFinalize.tsx
+++ packages/excalidraw/actions/actionFinalize.tsx
@@ -280,7 +280,7 @@ export const actionFinalize = register<FormData>({
 
     let selectedLinearElement =
       element && isLinearElement(element)
-        ? new LinearElementEditor(element, arrayToMap(newElements)) // To select the linear element when user has finished mutipoint editing
+        ? new LinearElementEditor(element, arrayToMap(newElements)) // To select the linear element when user has finished multipoint editing
         : appState.selectedLinearElement;
 
     selectedLinearElement = selectedLinearElement
